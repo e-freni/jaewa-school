631ca2c252895cb812fdd44a3a09755dd757034b
Enrico Freni <enrico.freni@jaewa.com>
2024-01-23 17:11:05 +0100

    configurazione corretta con testcontainers

--- src/test/java/com/example/demo/repository/TestRepositoryTest.java
+++ src/test/java/com/example/demo/repository/TestRepositoryTest.java
@@ -6,20 +6,44 @@ import com.example.demo.model.TestEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Testcontainers
+@ContextConfiguration(initializers = TestRepositoryTest.Initializer.class)
 class TestRepositoryTest {
+
+    @Container
+    public static MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:5.7.34")
+            .withDatabaseName("testdb")
+            .withUsername("root")
+            .withPassword("root");
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 
     @Autowired
     TestRepository testRepository;
 
     @Test
     void testPippoSave() {
-        this.testRepository.deleteAll();
         this.testRepository.save(new TestEntity("pippo"));
 
         List<TestEntity> all = this.testRepository.findAll();
